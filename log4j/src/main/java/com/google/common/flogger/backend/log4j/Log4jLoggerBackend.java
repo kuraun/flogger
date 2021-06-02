74b2856b505f2c0c6f16203a917a1d14e848be43
Flogger Team <java-team-github-bot@google.com>
2021-06-02 08:18:22 -0700

    Refactor Log4j backend - it has fewer allocation - replaced usage of deprecated SimpleMessageFormatter.format with SimpleMessageFormatter.getDefaultFormatter().format - removed the extra wasForced and isLoggable calls as they are being called before the backend is called - set logger fqcn as the logsite is not determined if it's already set on the logging event when creating it, which we do.
    RELNOTES=Refactor Log4j backend to streamline a bit and remove deprecated formatting usage.
    PiperOrigin-RevId: 377062530

--- log4j/src/main/java/com/google/common/flogger/backend/log4j/Log4jLoggerBackend.java
+++ log4j/src/main/java/com/google/common/flogger/backend/log4j/Log4jLoggerBackend.java
@@ -16,26 +16,15 @@
 
 package com.google.common.flogger.backend.log4j;
 
+import static com.google.common.flogger.backend.log4j.Log4jLoggingEventUtil.toLog4jLevel;
+import static com.google.common.flogger.backend.log4j.Log4jLoggingEventUtil.toLog4jLoggingEvent;
+
 import com.google.common.flogger.backend.LogData;
 import com.google.common.flogger.backend.LoggerBackend;
 import org.apache.log4j.Logger;
 
 /** A logging backend that uses log4j to output log statements. */
 final class Log4jLoggerBackend extends LoggerBackend {
-  /** Converts java.util.logging.Level to org.apache.log4j.Level. */
-  static org.apache.log4j.Level toLog4jLevel(java.util.logging.Level level) {
-    if (level.intValue() >= java.util.logging.Level.SEVERE.intValue()) {
-      return org.apache.log4j.Level.ERROR;
-    } else if (level.intValue() >= java.util.logging.Level.WARNING.intValue()) {
-      return org.apache.log4j.Level.WARN;
-    } else if (level.intValue() >= java.util.logging.Level.INFO.intValue()) {
-      return org.apache.log4j.Level.INFO;
-    } else if (level.intValue() >= java.util.logging.Level.FINE.intValue()) {
-      return org.apache.log4j.Level.DEBUG;
-    }
-    return org.apache.log4j.Level.TRACE;
-  }
-
   private final Logger logger;
 
   // VisibleForTesting
@@ -55,24 +44,15 @@ final class Log4jLoggerBackend extends LoggerBackend {
     return logger.isEnabledFor(toLog4jLevel(level));
   }
 
-  private void log(SimpleLogEvent logEntry, boolean wasForced) {
-    if (wasForced || logger.isEnabledFor(logEntry.getLevel())) {
-      forceLog(logger, logEntry);
-    }
-  }
-
   @Override
   public void log(LogData logData) {
-    log(SimpleLogEvent.create(logger, logData), logData.wasForced());
+    // The caller is responsible to call isLoggable() before calling this method to ensure that only
+    // messages above the given threshold are logged.
+    logger.callAppenders(toLog4jLoggingEvent(logger, logData));
   }
 
   @Override
   public void handleError(RuntimeException error, LogData badData) {
-    log(SimpleLogEvent.error(logger, error, badData), badData.wasForced());
-  }
-
-  private void forceLog(Logger logger, SimpleLogEvent logEntry) {
-    // Logger#callAppenders circumvents any evaluation of whether to log or not to log.
-    logger.callAppenders(logEntry.asLoggingEvent());
+    logger.callAppenders(toLog4jLoggingEvent(logger, error, badData));
   }
 }
